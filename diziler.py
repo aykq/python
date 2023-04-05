b677724cb0e319b66ae1e93d887ee9e1861af0a4
aykq <73436058+aykq@users.noreply.github.com>
2023-04-05 23:15:09 +0300

    yeni repo oluşturuldu

--- diziler.py
+++
@@ -1,8 +0,0 @@
-isimler = ["ahmet", "burak", "mehmet", "ugur"]
-
-print(isimler)
-print(isimler[0])
-print(isimler[1])
-print(isimler[2])
-
-print (len(isimler))
--- fonksiyonlar_metotlar.py
+++
@@ -1,6 +0,0 @@
-def isimleriListele():
-    isimler = ["ahmet", "burak", "mehmet", "ugur", "a", "b", "z"]
-    for i in range(len(isimler)):
-        print(isimler[i])
-        
-isimleriListele()

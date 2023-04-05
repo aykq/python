9c4e7b29b75e26b9f7a26e3ea2000120cbb9c25a
aykq <asdesra@gmail.com>
2023-04-05 23:50:07 +0300

    çalışmalar eklendi.

---
+++ learningPython/upper_lower.py
@@ -0,0 +1,22 @@
+str = "bi kac kelimelik string"
+print(len(str))
+
+
+# upper()
+
+print(str.upper())
+print(str.isupper())
+buyukYazdir = str.upper()
+buyukMu = str.isupper()
+print(buyukYazdir)
+print(buyukMu)
+
+
+# lower()
+
+print(str.lower())
+print(str.islower())
+kucukYazdir = str.lower()
+kucukMu = str.islower()
+print(kucukYazdir)
+print(kucukMu)

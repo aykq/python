c0400856c15c9bdcc54d9af706aa1e0a2230f5e4
aykq <asdesra@gmail.com>
2023-04-09 18:12:38 +0300

    çalışmalar eklendi.

--- learningPython/listeler - diziler.py
+++ learningPython/listeler - diziler.py
@@ -10,6 +10,19 @@ print(isimler[2])
 print (len(isimler))
 print(type(isimler))
 
+isimler = ["ahmet", "burak", "mehmet", "ugur"]
+isimler2 = ["fatma", "ayse", "mahmut", "oguz"]
 
-yeniListe = ["a", 19.2, 30, isimler]
+yeniListe = ["a", 19.2, 30, isimler2]
 print(type(yeniListe))
+print(type(yeniListe[0]))
+print(type(yeniListe[1]))
+print(type(yeniListe[2]))
+print(type(yeniListe[3]))
+
+tumListeler = [isimler, isimler2, "aa", "12.2", 12]
+print(tumListeler[:2])
+print(tumListeler[0:3])
+print(tumListeler[2:])
+print(tumListeler[1][3])
+print(tumListeler[0][1:])

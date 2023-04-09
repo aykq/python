af05909edd1e564689ef93f62165c9ec7a61d0ea
aykq <asdesra@gmail.com>
2023-04-09 17:59:44 +0300

    çalışmalar eklendi.

--- learningPython/listeler - diziler.py
+++ learningPython/listeler - diziler.py
@@ -1,3 +1,5 @@
+# [] veya list() kullanarak liste oluşturulabilir
+
 isimler = ["ahmet", "burak", "mehmet", "ugur"]
 
 print(isimler)
@@ -6,3 +8,8 @@ print(isimler[1])
 print(isimler[2])
 
 print (len(isimler))
+print(type(isimler))
+
+
+yeniListe = ["a", 19.2, 30, isimler]
+print(type(yeniListe))

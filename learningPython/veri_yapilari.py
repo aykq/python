039609c7b3c7758e782a74b85c40ee0408d03f30
aykq <asdesra@gmail.com>
2023-04-10 17:48:42 +0300

    çalışmalar eklendi.

---
+++ learningPython/veri_yapilari.py
@@ -0,0 +1,14 @@
+# tuple
+# sıralıdır, değiştirilemez
+
+t = ("ali", "veli", 1, 2, 3, [4, 5, 6]) # üç şekilde de (satır 4-5-6) tuple oluşturulabilir
+t = "ali", "veli", 1, 2, 3, [4, 5, 6]
+t = ("test",)  # virgüle dikkat
+print(type(t))
+
+#--------------------------------------------------------
+
+# dictionary - sozluk 
+# sırasız, değiştirilebilir
+# listelerdeki gibi indexleme yapılamaz
+

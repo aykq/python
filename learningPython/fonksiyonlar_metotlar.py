11f2c32e2324e0479b6437afcc8e364e513d022f
aykq <asdesra@gmail.com>
2023-04-12 05:15:04 +0300

    çalışmalar eklendi.

--- learningPython/fonksiyonlar_metotlar.py
+++ learningPython/fonksiyonlar_metotlar.py
@@ -2,12 +2,26 @@ def isimleriListele():
     isimler = ["ahmet", "burak", "mehmet", "ugur", "a", "b", "z"]
     for i in range(len(isimler)):
         print(isimler[i])
-        
+
+
 isimleriListele()
 
 # --------------------------------------------------------
 
+
 def kareAl(x):
-    print("girilen " + str(x) +  " sayisinin karesi: " + str(x**2))
-    
+    print("girilen " + str(x) + " sayisinin karesi: " + str(x**2))
+
+
 kareAl(5)
+
+# --------------------------------------------------------
+
+# 2 argümanlı fonksiyon
+def carpma(x, y):
+    print("sonuc: " + str(x*y))
+    
+carpma(3, 5)
+
+# --------------------------------------------------------
+

b7ffb5c195b1f6d94f4aef594d9f5453d77b80f2
aykq <asdesra@gmail.com>
2023-04-13 22:09:53 +0300

    çalışmalar eklendi.

--- learningPython/fonksiyonlar_metotlar.py
+++ learningPython/fonksiyonlar_metotlar.py
@@ -18,7 +18,7 @@ kareAl(5)
 # 2 argümanlı fonksiyon
 def carpma(x, y):
     print("sonuc: " + str(x*y))
-    
+
 carpma(3, 5)
 
 # --------------------------------------------------------
@@ -26,6 +26,30 @@ carpma(3, 5)
 # 2 argümanlı fonksiyon - return ile
 def carpma(x, y):
     return x*y
-    
+
 cikti = carpma(3, 5)
 print(cikti)
+
+# --------------------------------------------------------
+
+# local ve global değişkenler
+x = 20
+y = 30      # bunlar global değişken
+
+def carp(x, y):     # metot parametresi olarak verilen değişkenlerveya döngü değişkenleri local değişkendir
+    print(x*y)
+
+# --------------------------------------------------------
+
+# local ==>> global değişken
+
+x = []
+
+def elemanEkle(y):
+    x.append(y)
+    print(str(y) + " ifadesi eklendi")
+    print(x)
+    
+elemanEkle(3)
+elemanEkle(2)
+elemanEkle("ali")

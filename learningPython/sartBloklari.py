e3557b43cf0bda646bf5d396a95cd3651387c6b0
aykq <asdesra@gmail.com>
2023-04-17 12:32:46 +0300

    çalışmalar eklendi.

--- learningPython/sartBloklari.py
+++ learningPython/sartBloklari.py
@@ -2,11 +2,27 @@ sayi1 = 5
 sayi2 = 5
 
 if sayi1 > sayi2:
-  print("birinci sayi daha buyuk - " + sayi1)
+    print("birinci sayi daha buyuk - " + sayi1)
+
 
 elif sayi1 < sayi2:
-  print("ikinci sayi daha buyuk - " + sayi2)
+    print("ikinci sayi daha buyuk - " + sayi2)
+
 
 else:
-  print("sayilar esit")
-  
+    print("sayilar esit")
+
+# --------------------------------------------------------
+
+sinir = 50000
+magazaAdi = input("magaza adi nedir?")
+gelir = int(input("magazanin geliri: "))
+
+if gelir > sinir:
+    print("gelir, sinirdan yuksek")
+
+elif gelir < sinir:
+    print("gelir, sinirdan dusuk")
+
+else:
+    print("gelir, sinir deger ile ayni")

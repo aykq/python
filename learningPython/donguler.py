ba2d70aa4dd75b578fde83c1013b035602f2bbbf
aykq <asdesra@gmail.com>
2023-04-20 08:17:34 +0300

    çalışmalar eklendi.

--- learningPython/donguler.py
+++ learningPython/donguler.py
@@ -33,10 +33,12 @@ for i in range(2, 31, 2):  # başlangıç 2, bitiş 31. 2 arttırarak say.
 
 maaslar = [1000, 2000, 3000, 4000]
 
+
 def zamliMaas(x):
     print(x*20/100 + x)
 
 # zamliMaas(2000)
+
 
 for i in maaslar:
     print(zamliMaas(i))
@@ -47,18 +49,54 @@ for i in maaslar:
 # maas > 3000 - %10 zam
 # maas < 3000 - %20 zam
 
-maaslar = [1000, 2000, 4000, 5000]
+maaslar = [1000, 2000, 3000, 4000, 5000]
+
 
 def yuzdeOn(x):
     print(x*10/100 + x)
-    
+
+
 def yuzdeYirmi(x):
     print(x*20/100 + x)
-    
+
+
 for i in maaslar:
-    if i > 3000:
+    if i >= 3000:
         yuzdeOn(i)
-        
+
     else:
         yuzdeYirmi(i)
-        
+
+# --------------------------------------------------------
+
+# break ve continue
+
+maaslar = [8000, 5000, 2000, 1000, 3000, 7000, 1000]
+maaslar.sort()
+print(maaslar)
+
+for i in maaslar:       # break kullanımı
+    if i == 3000:
+        print("donguden cikildi")
+        break
+    
+    else:
+        print(i)
+    
+    
+for i in maaslar:       # continue kullanımı
+    if i == 3000:
+        print(str(i) + " degeri atlandi")
+        continue
+    
+    else:
+        print(i)
+    
+# --------------------------------------------------------
+
+# while
+
+sayi = 0
+while sayi < 10:
+    sayi += 1
+    print(sayi)
